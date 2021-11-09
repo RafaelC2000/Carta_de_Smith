c97d3917f718f4958e03d7c2fc439e0befaa04ad
Ernesto Cedillo <rafacrteto@outlook.com>
2021-11-09 01:09:51 -0600

    Rotacion del punto

--- JS/circle.js
+++ JS/circle.js
@@ -8,6 +8,8 @@ $normal=document.querySelector(".normal")
 $point=document.getElementById("point")
 $coords=document.getElementById("coords")
 $new=document.querySelector(".newCircle")
+$landa=document.getElementById("landa")
+$point2=document.getElementById("point2")
 
 $send.addEventListener('click',() => {
     let z0 = $impedance.value
@@ -38,7 +40,7 @@ function normalize (z,r,i) {
     }else{
         z="+"
     }
-    $normal.innerText = `${r.toFixed(3)} ${z} i${(Math.abs(i)).toFixed(3)}&#937`
+    $normal.innerText = `${r.toFixed(3)} ${z} i${(Math.abs(i)).toFixed(3)}Ω`
     r=1/(1+r)
     i=1/i
     setCoords(r,i)
@@ -88,19 +90,232 @@ function findPoint(a,h,b,k,r,i){
         y=((-v)+Math.sqrt(Math.pow(v,2)-4*1*w))/(2*1)
     }
     let x=(o-y*q)/p
-    console.log(x,y)
     let point = `translate(${x}px, ${-(y)}px)`
+    let sX, sY
+
     $point.style.setProperty("transform", point)
-
     $coords.style.setProperty("transform", point)
-    $coords.innerText = `(${((1/r)-1).toFixed(3)},${(1/i).toFixed(3)})`
+    $coords.innerText = `(${((1/r)-1).toFixed(3)},${(1/i).toFixed(3)})`;
 
-    newCircle(Math.abs(500-x),Math.abs(500-y))
+    (x<500) ? sX=false : sX=true;
+    (y<500) ? sY=false : sY=true;
+
+    console.log(`x=${x}, y=${y}, sx ${sX}, sy ${sY}`)
+
+    newCircle(Math.abs(500-x),Math.abs(500-y),sX,sY)
 }
 
-function newCircle(x,y){
+function newCircle(x,y,sX,sY){
     let r=Math.sqrt(Math.pow(x,2) + Math.pow(y,2))
     $new.style.setProperty("width", `${2*r}px`)
     $new.style.setProperty("height", `${2*r}px`)
-    console.log(r)
+    landa(r,x,y,sX,sY)
+}
+
+function landa(r,x,y,sX,sY) {
+    let a,p,q,l,m,n;
+    (sX) ? x : x*=-1;
+    (sY) ? y : y*=-1;
+    a=Math.atan(Math.abs(y/x))
+    a=(a*180)/Math.PI
+    l=($landa.value%0.5)*720
+    console.log(`Landa: ${l}°`)
+    console.log(`Radio: ${r}`)
+    if(sX==false && sY==true){
+        a+=0
+        console.log(`1. ${a}°`)
+        a+=l
+        console.log(`A ${a}°`)
+        if((a>=0 && a<90)||(a>=360 && a<450)){
+            a=a%360
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500-p
+            n=500+q
+        }else if((a>=90 && a<180)||(a>=450 && a<540)){
+            a=a%360
+            a=180-a
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500+p
+            n=500+q
+        }else if((a>=180 && a<270)||(a>=540 && a<630)){
+            a=a%360
+            a-=180
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500+p
+            n=500-q
+        }else if((a>=270 && a<360)||(a>=630 && a<720)){
+            a=a%360
+            a=360-a
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500-p
+            n=500-q
+        }
+        console.log(`x: ${x}, y: ${y}`)
+        console.log(`p: ${p}, q: ${q}`)
+        console.log(`m: ${m}, n: ${n}`)
+    }else if(sX==true && sY==true){
+        a=180-a
+        console.log(`2. ${a}°`)
+        a+=l
+        console.log(`A ${a}°`)
+        if((a>=0 && a<90)||(a>=360 && a<450)){
+            a=a%360
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500-p
+            n=500+q
+        }else if((a>=90 && a<180)||(a>=450 && a<540)){
+            a=a%360
+            a=180-a
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500+p
+            n=500+q
+        }else if((a>=180 && a<270)||(a>=540 && a<630)){
+            a=a%360
+            a-=180
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500+p
+            n=500-q
+        }else if((a>=270 && a<360)||(a>=630 && a<720)){
+            a=a%360
+            a=360-a
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500-p
+            n=500-q
+        }
+        console.log(`x: ${x}, y: ${y}`)
+        console.log(`p: ${p}, q: ${q}`)
+        console.log(`m: ${m}, n: ${n}`)
+    }else if(sX==true && sY==false){
+        a+=180
+        console.log(`3, ${a}°`)
+        a+=l
+        console.log(`A ${a}°`)
+        if((a>=0 && a<90)||(a>=360 && a<450)){
+            a=a%360
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500-p
+            n=500+q
+        }else if((a>=90 && a<180)||(a>=450 && a<540)){
+            a=a%360
+            a=180-a
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500+p
+            n=500+q
+        }else if((a>=180 && a<270)||(a>=540 && a<630)){
+            a=a%360
+            a-=180
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500+p
+            n=500-q
+        }else if((a>=270 && a<360)||(a>=630 && a<720)){
+            a=a%360
+            a=360-a
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500-p
+            n=500-q
+        }
+        console.log(`x: ${x}, y: ${y}`)
+        console.log(`p: ${p}, q: ${q}`)
+        console.log(`m: ${m}, n: ${n}`)
+    }else if(sX==false && sY==false){
+        a=360-a
+        console.log(`4. ${a}°`)
+        a+=l
+        console.log(`A ${a}°`)
+        if((a>=0 && a<90)||(a>=360 && a<450)){
+            a=a%360
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500-p
+            n=500+q
+        }else if((a>=90 && a<180)||(a>=450 && a<540)){
+            a=a%360
+            a=180-a
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500+p
+            n=500+q
+        }else if((a>=180 && a<270)||(a>=540 && a<630)){
+            a=a%360
+            a-=180
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500+p
+            n=500-q
+        }else if((a>=270 && a<360)||(a>=630 && a<720)){
+            a=a%360
+            a=360-a
+            console.log(`A ${a}°`)
+            a=(a*Math.PI)/180
+            console.log(Math.cos(a))
+            p=r*Math.cos(a)
+            q=r*Math.sin(a)    
+            m=500-p
+            n=500-q
+        }
+        console.log(`x: ${x}, y: ${y}`)
+        console.log(`p: ${p}, q: ${q}`)
+        console.log(`m: ${m}, n: ${n}`)
+    }
+
+    let point = `translate(${m}px, ${-(n)}px)`
+
+    $point2.style.setProperty("transform", point)
 }
